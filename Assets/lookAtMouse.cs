127b06ab5f62079cea48c6ee29c8efb8396c8a3b
MUKILTEO\1725383 <189248david@gmail.com>
2025-10-22 12:46:46 -0700

    player looks at mouse

---
+++ Assets/lookAtMouse.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class lookAtMouse : MonoBehaviour
+{
+
+    float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LAmouse();
+        Debug.Log(angle);
+    }
+
+    private void LAmouse()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Vector3 mousePos = hitInfo.point;
+            mousePos.y = transform.position.y;
+
+            transform.forward = mousePos - transform.position;
+        }
+
+    }
+}
